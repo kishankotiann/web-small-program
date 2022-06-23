c0b41039b8afb6a34e14a839a0c1a449ad2c4470
kishan kotian <106797610+kishankotiann@users.noreply.github.com>
2022-06-23 12:26:48 +0530

    product of 2 number

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ class sum
   public static void main(String args[])
   {
     int a=10,b=20;
-    system.out.println("sum of 2 number="+(a+b);
+    system.out.println("product of 2 number="+(a*b);
        }
          }
